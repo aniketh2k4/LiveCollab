16e6cea3023391ceab64954ea4a34d547fc751f7
J Aniketh <anikethjm13@gmail.com>
2025-08-22 19:21:03 +0530

    Initial commit

---
+++ client/src/components/GitHubCorner.tsx
@@ -0,0 +1,24 @@
+
+import useWindowDimensions from "@/hooks/useWindowDimensions"
+
+function GitHubCorner() {
+    
+    const { } = useWindowDimensions()
+
+    return (
+        <a
+            href=""
+            className="github-corner"
+            aria-label="View source on GitHub"
+            target="_blank"
+            rel="noreferrer"
+            style={{
+                
+            }}
+        >
+            
+        </a>
+    )
+}
+
+export default GitHubCorner
